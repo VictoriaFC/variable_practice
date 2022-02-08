929bedc6534ba7658a2dae3e214baec76b93a2e3
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-02-08 11:16:38 -0700

    Reassign variables

--- variables.rb
+++ variables.rb
@@ -9,3 +9,11 @@ var dogAge2 = 3;
 var dogWeight = 50.5;
 var dogWeight1 = 16.4;
 var dogWeight2 = 60.7;
+
+var isDogChubby = true;
+var isDogChubby1 = false;
+var isDogChubby2 = false;
+
+dogName = "Buddy";
+dogName1 = "Peaches";
+dogName2 = "Spot";
