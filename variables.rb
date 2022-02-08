31bbcdf617949203219225a496fab924e1a4a9f4
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-02-08 11:23:32 -0700

    Add code annotations

--- variables.rb
+++ variables.rb
@@ -2,18 +2,22 @@ var dogName = "Buster";
 var dogName1 = "Meadow";
 var dogName2 = "Bayson";
 
+# variable dogAge is declared adn assigned to an integer to represent that dogs age
 var dogAge = 4;
 var dogAge1 = 2;
 var dogAge2 = 3;
 
+# variable dogWeight is declared by floats
 var dogWeight = 50.5;
 var dogWeight1 = 16.4;
 var dogWeight2 = 60.7;
 
+# variable isDogChubby is declared and assigned to a boolean
 var isDogChubby = true;
 var isDogChubby1 = false;
 var isDogChubby2 = false;
 
+# variable dogName is reassigned to different dog names with a string
 dogName = "Buddy";
 dogName1 = "Peaches";
 dogName2 = "Spot";
